bb4fe9916244d04a25e429a753d32d1098044406
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-03-11 23:41:09 +0530

    Qno1:450 question to reverse array using the iterative as well as recursive way

---
+++ src/Array/Reverse_array.java
@@ -0,0 +1,46 @@
+package Array;
+
+public class Reverse_array {
+
+    //TODO iterative solution
+    private static void reversearrayiter1(int[] arr) {
+        int startindex = 0;
+        int endindex = arr.length - 1;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[startindex];
+            arr[startindex] = arr[endindex];
+            arr[endindex] = temp;
+            startindex++;
+            endindex--;
+        }
+//Time Complexity O(n)
+    }
+
+    private static void reversearrayiter2(int[] arr) {
+        int startindex = 0;
+        int endindex = arr.length - 1;
+        while (startindex < endindex) {
+            int temp = arr[startindex];
+            arr[startindex] = arr[endindex];
+            arr[endindex] = temp;
+            startindex++;
+            endindex--;
+        }
+
+    }
+//TODO Recursive solution
+
+    private static int[] reversearrayRecursive(int[] arr, int start, int end) {
+        if (start >= end)
+            return arr;
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        return reversearrayRecursive(arr, start + 1, end - 1);
+
+        //Time Complexity O(n)
+    }
+
+
+}
--- src/Solution.java
+++ src/Solution.java
@@ -6,6 +6,19 @@ public class Solution {
 
     public static void main(String[] args) {
 
+        int arr[] = new int[]{2, 3, 4, 5, 6};
+
+        printArray(reversearray(arr, 0, arr.length - 1));
+    }
+
+    private static int[] reversearray(int[] arr, int start, int end) {
+
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 
 
