e48c5dd2eb9db8e18f7f1cf53f90315777eba328
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-03-20 21:55:26 +0530

    in the making of intersection of two arrays

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int[] num1 = new int[]{1, 2, 2, 1, 4, 6, 7, 8, 8};
+        int[] num2 = new int[]{2, 2, 8, 4, 6};
+        int[] result = intersection(num1, num2);
+        for (int j : result) System.out.println(j);
     }
-}
+
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        ArrayList<Integer> arr = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                if (!arr.contains(nums1[i])) {
+                    arr.add(nums1[i]);
+                    i++;
+                    j++;
+                }
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        int[] res = new int[arr.size()];
+        for (int h = 0; h < arr.size(); h++) {
+            res[h] = arr.get(h);
+        }
+        return res;
+
+    }
+
 }
