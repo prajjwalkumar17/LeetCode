bc74f0deba95af5d558f1adcb81768ab152aca95
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-03-22 20:11:40 +0530

    Made the array rotate by one position and even made the array rotate by d positions by 3 different methods 1st one takes theta nd time second method takes theta n and theta d of aux space and 3rd will only take n time complexity and no aux space

--- src/Array/Rotating_an_array.java
+++ src/Array/Rotating_an_array.java
@@ -6,7 +6,7 @@ package Array;
         rotate 2 steps to the right: [6,7,1,2,3,4,5]
         rotate 3 steps to the right: [5,6,7,1,2,3,4]*/
 public class Rotating_an_array {
-    public static void rotate(int[] nums, int k) {
+ /*   public static void rotate(int[] nums, int k) {
         for(int p=nums.length-1;p>= nums.length-k;p--){
             int temp=nums[p];
             for(int u=p-1;u>=0;u--){
@@ -18,5 +18,37 @@ public class Rotating_an_array {
         for (int a=0;a< nums.length;a++){
             System.out.println(nums[a]);
         }
+    }*/
+
+
+    //TODO 1st approach Theta(nd)
+    private static int[] rotateArrayDPos(int[] arr, int d) {
+        for (int i = 0; i < d; i++)
+            rotatearray(arr);
+        return arr;
+    }
+
+    private static int[] rotatearray(int[] arr) {
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[arr.length - 1] = temp;
+        return arr;
+    }
+
+
+    //TODO 2nd Approach
+    private static int[] rotateArrayDPosauxD(int[] arr, int d) {
+        int[] tempArr = new int[d];
+        for (int j = 0; j < d; j++)
+            tempArr[j] = arr[j];
+
+        for (int i = d; i < arr.length; i++)
+            arr[i - d] = arr[i];
+
+        for (int i = 0; i < d; i++)
+            arr[arr.length - d + i] = tempArr[i];
+        return arr;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,22 @@ public class Main {
     public static void main(String[] args) {
 
         int[] num1 = new int[]{1, 1, 0, 0, 8, 0, 9};
-
+        printArray(rotateArrayDPosauxD(num1, 2));
 
     }
 
+    private static int[] rotateArrayDPosauxD(int[] arr, int d) {
+        int[] tempArr = new int[d];
+        for (int j = 0; j < d; j++)
+            tempArr[j] = arr[j];
+
+        for (int i = d; i < arr.length; i++)
+            arr[i - d] = arr[i];
+
+        for (int i = 0; i < d; i++)
+            arr[arr.length - d + i] = tempArr[i];
+        return arr;
+    }
 
     public static void printArray(int[] array) {
         for (int j : array) {
