95d126b9474f0fad9ff137fc15c369da159113e0
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-03-24 22:21:53 +0530

    Made rotate array by d pos right as well as left by all the methods 1. by reversing 1 time by d times 2. by copying to an auxilary array 3. by juggling

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] num1 = new int[]{-1, 2};
+        int[] num1 = new int[]{-3};
+        System.out.println(MaximumSubarraykaadane(num1));
 
+    }
 
+    private static int MaximumSubarraykaadane(int[] nums) {
+        int result = nums[0];
+        int curr = 1;
+        for (int num : nums) {
+            curr = Math.max(curr * num, num);
+            result = Math.max(curr, result);
+        }
+        return result;
     }
 
 
