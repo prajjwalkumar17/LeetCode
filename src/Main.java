d926f7aa813f59bfb8f438c3215301a3077e6f84
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-03 14:28:09 +0530

    made the max consec1 problem with NAIVE idea as nested for loop with i= 0 and j =i and breaking the inner loop as soon as 0 occurs and after that comcalc res
    and EFFF idea as only once traversing the array once and incrementing count1 everytime except when 0 occurs then comcalc res and set curr1 to 0

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         int[] num1 = new int[]{1, 0, 1, 1, 1, 1, 0, 1, 1};
-        System.out.println(maxConsec1Eff(num1));
 
 
     }
