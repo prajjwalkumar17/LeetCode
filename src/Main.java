5221cbd60499e82f1dd2ea3f144a318e7980838e
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-05 23:06:37 +0530

    Made the leetCode water container problem and searching in a 2D sorting matrix using Binary search

--- src/Main.java
+++ src/Main.java
@@ -3,45 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] num1 = new int[][]{
-                {1, 3, 5, 7},
-                {10, 11, 16, 20},
-                {23, 30, 34, 50}};
-        searchElemEFFF(num1, 10);
+        int[] num1 = new int[]{10, 11, 16, 20};
     }
-
-    private static void searchElemEFFF(int[][] matrix, int target) {
-        int colNo = 0;
-        for (int i = matrix[0].length - 1; i >= 0; i--) {
-            if (matrix[0][i] <= target) {
-                colNo = i;
-                break;
-            }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][colNo] == target) {
-                return true;
-            }
-        }
-        return false;
-    }
-//
-//    private static boolean searchElemEFFF(int[][] matrix, int target) {
-//        int colNo = 0;
-//        for (int i = matrix[0].length-1; i >=0; i--) {
-//            if (matrix[0][i] < target) {
-//                colNo = i;
-//                break;
-//            }
-//        }
-//        System.out.println(colNo);
-//        for(int i=0;i<matrix.length;i++){
-//            if(matrix[i][colNo]==target){
-//                return true;
-//            }
-//        }
-//       return false;
-//    }
 
 
     public static void printArray(int[] nums) {
