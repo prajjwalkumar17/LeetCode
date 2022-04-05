36fbb2bbe83f79bd4e37d74a8d6fbef3da03ac9a
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-05 13:49:43 +0530

    Made the reshaping of matrix problem

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] num1 = new int[]{1, 8, 30, -5, 20, 7};
+        int[] num1 = new int[]{1, 2, 3, 4};
+
 
     }
 
---
+++ src/Matrix/ReshapeTheMatrix.java
@@ -0,0 +1,27 @@
+package Matrix;
+
+public class ReshapeTheMatrix {
+/*
+    int[][] num1 = new int[][]{{1, 2},{3,4}};
+    int[][] mat=matrixReshape(num1,3,2);
+        for(int[] row:mat){
+        for(int col:row){
+            System.out.print(col);
+        }
+        System.out.println();
+    }
+*/
+
+
+//    public static int[][] matrixReshape(int[][] mat, int r, int c) {
+//        int m = mat.length, n = mat[0].length;
+//        if (r * c != m * n)
+//            return mat;
+//
+//        int[][] reshaped = new int[r][c];
+//        for (int i = 0; i < r * c; i++)
+//            reshaped[i/c][i%c] = mat[i/n][i%n];
+//
+//        return reshaped;
+//    }
+}
