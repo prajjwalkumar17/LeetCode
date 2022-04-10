d5c28f746c95eb8a5d22b1c96a13f1d5e93b0deb
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-10 21:01:21 +0530

    Made the queue implementation from stack

---
+++ src/Stack/QueueFromStack.java
@@ -0,0 +1,44 @@
+package Stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class QueueFromStack {
+    Deque<Integer> s1 = new LinkedList<>();
+    Deque<Integer> s2 = new LinkedList<>();
+
+    public QueueFromStack() {
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        if (s2.isEmpty()) {
+            if (s1.isEmpty()) return -1;
+            else {
+                while (!s1.isEmpty()) {
+                    int data = s1.pop();
+                    s2.push(data);
+                }
+                return (s2.pop());
+
+            }
+        } else {
+            return (s2.pop());
+        }
+    }
+
+    public int peek() {
+        if (!s2.isEmpty())
+            return s2.peek();
+        else return s1.peekLast();
+    }
+
+
+    public boolean empty() {
+        if (s2.isEmpty() && s1.isEmpty()) return true;
+        else return false;
+    }
+}
