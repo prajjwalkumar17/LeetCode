f4c125f40577555247e22ad3feefe11580554242
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-12 02:07:54 +0530

    Made the problem of finding nodes at distance k from root

---
+++ src/Tree/NodesAtDistanceK.java
@@ -0,0 +1,15 @@
+package Tree;
+
+public class NodesAtDistanceK {
+
+//    void NodesAtK(Node root,int k){
+//        if(root==null) return;
+//        if(k==0) System.out.println(root.val);
+//        else{
+//            NodesAtK(root.left,k-1);
+//            NodesAtK(root.right,k-1);
+//        }
+//
+//    }
+
+}
