6873ec1b294f0277296ae37ebbec51888790a940
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-14 02:14:58 +0530

    Made the insertion and searching in the binary search tree

---
+++ src/BinarySearchTree/InsertintoBST.java
@@ -0,0 +1,20 @@
+package BinarySearchTree;
+
+//You are given the root node of a binary search tree (BST) and a value to insert into the tree. Return the
+//        root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+//
+//        Notice that there may exist multiple valid ways
+//        for the insertion, as long as the tree remains a BST after
+//        insertion. You can return any of them.
+public class InsertintoBST {
+//    public TreeNode insertIntoBST(TreeNode root, int val) {
+//
+//        if(root ==null)return new TreeNode(val);
+//        if(root.val>val)root.left=insertIntoBST(root.left,val);
+//        if(root.val<val)root.right=insertIntoBST(root.right,val);
+//        return root;
+//    }
+//}
+
+
+}
---
+++ src/BinarySearchTree/SearchInBST.java
@@ -0,0 +1,21 @@
+package BinarySearchTree;
+
+public class SearchInBST {
+//    Basically as the tree is a BST we just need to **check wheather the root of tree contains the val or not** if not we compare the val with the root
+//* if **val>root.val** then we traverse right side of tree
+//* if **val<root.val** we travel left side of tree recursively
+//* else if **root becomes null** that means key isn't there we return null
+//
+//    Hit upvote all hungrycoders out there if it fulfilled your hunger❤❤❤
+//
+//            ```
+//    class Solution {
+//        public TreeNode searchBST(TreeNode root, int val) {
+//            if(root==null || root.val==val)return root;
+//            if(val<root.val)return searchBST(root.left,val);
+//            if(val>root.val)return searchBST(root.right,val);
+//            return null;
+//        }
+//    }
+//```
+}
