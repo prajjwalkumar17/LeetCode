70ea5cafb624b635e752ca4d766d82546bfe472b
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-14 10:09:36 +0530

    Made the validate ques of lC

---
+++ src/BinarySearchTree/ValidateABST.java
@@ -0,0 +1,14 @@
+package BinarySearchTree;
+
+public class ValidateABST {
+//    public boolean isValidBST(TreeNode root) {
+//        return isValidBST(root,-Long.MAX_VALUE,Long.MAX_VALUE);
+//    }
+//
+//    public boolean isValidBST(TreeNode root,long min,long max) {
+//        if(root==null)return true;
+//        if(root.val>max||root.val<min)return false;
+//        else return isValidBST(root.left,min,root.val) &&
+//                isValidBST(root.right,root.val,max);
+//    }
+}
