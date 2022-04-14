0eae9fdbdfd9769fb2192c15b099e9c331b99a15
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-14 10:15:32 +0530

    made the Lowest Common ansestor problem of BST of LC

---
+++ src/BinarySearchTree/LCAofBST.java
@@ -0,0 +1,14 @@
+package BinarySearchTree;
+
+public class LCAofBST {
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        //3 cases arises
+//        //1 root will have one node on left and one on right return root
+//        //2 root will have value as one of the key
+//        //3 root will have both the values on one side
+//        if(root==null)return root;
+//        if(root.val >p && root.val>q)lowestCommonAncestor(root.left,p,q);
+//        if(root.val <p && root.val<q)lowestCommonAncestor(root.right,p,q);
+//        else return root;
+//    }
+}
