31d1a563d60ceae4e6148a75d5f943f067c8063d
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-14 21:45:48 +0530

    Made the 2sum problem for BST

---
+++ src/BinarySearchTree/sum2searchpair.java
@@ -0,0 +1,20 @@
+package BinarySearchTree;
+
+
+//Given the root of a Binary Search Tree and a target number k, return true if
+//        there exist two elements in the BST such that their sum is equal to the
+//        given target.
+public class sum2searchpair {
+//
+//    public boolean findTarget(TreeNode root, int k) {
+//        Set<Integer> map=new HashSet<>();
+//        return addToMap(root,k,map);
+//    }
+//    boolean addToMap(TreeNode root,int k,Set map){
+//        if(root==null)return false;
+//        if(map.contains(k-root.val))return true;
+//        else map.add(root.val);
+//        return (addToMap(root.left,k,map) || addToMap(root.right,k,map));
+//    }
+
+}
