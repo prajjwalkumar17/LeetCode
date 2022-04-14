de5f3b432515f7d60156540caf2282ded3e58ed2
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-14 10:30:42 +0530

    Made the max val fining in a tree

--- src/Tree/MaxValInBinaryTree.java
+++ src/Tree/MaxValInBinaryTree.java
@@ -3,6 +3,11 @@ package Tree;
 public class MaxValInBinaryTree {
     //    class TreeNode
 
+//    int getMax(TreeNode root){
+//        if(root==null)return Integer.MIN_VALUE;
+//        else return(Math.max(root.val,
+//                Math.max(getMax(root.left), getMax(root.right))))
+//    }
 
 
 }
