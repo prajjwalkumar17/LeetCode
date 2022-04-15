690a39235735c6149efcf290291059cc0e426b6e
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-16 01:17:21 +0530

    Made the selection Sort

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+public class SelectionSort {
+    static int[] Selectionsort(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[j] < arr[min_index])
+                    min_index = j;
+                swap(arr, i, min_index);
+            }
+        }
+        return arr;
+
+    }
+
+    static int[] swap(int[] arr, int arr1, int arr2) {
+        int tmp = arr[arr1];
+        arr[arr1] = arr[arr2];
+        arr[arr2] = tmp;
+        return arr;
+    }
+}
