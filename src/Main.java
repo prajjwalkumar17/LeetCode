193e170759bb04f163d531fa3dbab511563d0ec1
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-15 22:16:49 +0530

    Made the bubble sort algo

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,24 @@ public class Main {
     public static void main(String[] args) {
         int[] num1 = new int[]{11, 22, 13, 84};
         char[] num2 = new char[]{'A', 'B', 'a', 'c', 'D'};
+        System.out.println(Arrays.toString(bubblesort(num1)));
 
 
     }
 
+    static int[] bubblesort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+
+        return arr;
+    }
+
+    static int[] swap(int[] arr, int arr1, int arr2) {
+        int tmp = arr[arr1];
+        arr[arr1] = arr[arr2];
+        arr[arr2] = tmp;
+        return arr;
+    }
 
 }
--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -2,4 +2,19 @@ package Sorting;
 
 public class BubbleSort {
 
+    int[] bubblesort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++)
+            for (int j = 0; j < arr.length - i - 1; j++)
+                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+
+        return arr;
+    }
+
+    int[] swap(int[] arr, int arr1, int arr2) {
+        int tmp = arr[arr1];
+        arr[arr1] = arr[arr2];
+        arr[arr2] = tmp;
+        return arr;
+    }
+
 }
