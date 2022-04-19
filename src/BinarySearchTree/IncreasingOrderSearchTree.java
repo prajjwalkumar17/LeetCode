0110198ebb37dfa62ae69654ba55312eb4256736
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-19 19:57:12 +0530

    increasing order BST

--- src/BinarySearchTree/IncreasingOrderSearchTree.java
+++ src/BinarySearchTree/IncreasingOrderSearchTree.java
@@ -1,5 +1,6 @@
 package BinarySearchTree;
-
+//897 Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the
+//        root of the tree, and every node has no left child and only one right child.
 public class IncreasingOrderSearchTree {
 }
 
@@ -18,20 +19,31 @@ class TreeNode {
         this.right = right;
     }
 
-//    class Solution {
-//        private TreeNode newRoot = new TreeNode(0);
-//        private TreeNode tracker = newRoot;
-//        public TreeNode increasingBST(TreeNode root) {
+    //TOdo to have a look how the recurssion works here
+
+
 //
-//            if(root == null)
-//                return root;
-//
-//            increasingBST(root.left);
-//            tracker.right = root;
-//            tracker = tracker.right;
-//            root.left = null;
-//            increasingBST(root.right);
-//            return newRoot.right;
-//        }
+//    //                subsets(num1);
+//    public static List<List<Integer>> subsets(int[] nums) {
+//        List<List<Integer>> res = new ArrayList<>();
+//        subsets(nums, new ArrayList<Integer>(), 0, res);
+//        return res;
 //    }
+//    public static void subsets(int[] nums, List<Integer> curr, int i, List<List<Integer>> res) {
+//        if (i == nums.length) {
+//            res.add(new ArrayList<>(curr)); // copy of curr is being added
+//            return;
+//        }
+//        System.out.println(curr+"/////////////0");
+//        curr.add(nums[i]);
+//        System.out.println(curr+"/////////////1");
+//        subsets(nums,curr,i+1,res);
+//        System.out.println(curr+"/////////////2");
+//        curr.remove(curr.size()-1);
+//        System.out.println(curr+"/////////////3");
+//
+//        subsets(nums, curr, i + 1, res);
+//        System.out.println(curr+"/////////////4");
+//    }
+//
 }
--- src/Main.java
+++ src/Main.java
@@ -11,33 +11,7 @@ public class Main {
     }
 
 
-    //TOdo to have a look how the recurssion works here
 
-
-//
-//    //                subsets(num1);
-//    public static List<List<Integer>> subsets(int[] nums) {
-//        List<List<Integer>> res = new ArrayList<>();
-//        subsets(nums, new ArrayList<Integer>(), 0, res);
-//        return res;
-//    }
-//    public static void subsets(int[] nums, List<Integer> curr, int i, List<List<Integer>> res) {
-//        if (i == nums.length) {
-//            res.add(new ArrayList<>(curr)); // copy of curr is being added
-//            return;
-//        }
-//        System.out.println(curr+"/////////////0");
-//        curr.add(nums[i]);
-//        System.out.println(curr+"/////////////1");
-//        subsets(nums,curr,i+1,res);
-//        System.out.println(curr+"/////////////2");
-//        curr.remove(curr.size()-1);
-//        System.out.println(curr+"/////////////3");
-//
-//        subsets(nums, curr, i + 1, res);
-//        System.out.println(curr+"/////////////4");
-//    }
-//
 
 
 }
