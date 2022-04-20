2787613b3c2f1c2c7d02a1ddaff99614e1e07adf
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-20 22:07:19 +0530

    made the program to delete mid node

--- src/LinkedList/MidElem.java
+++ src/LinkedList/MidElem.java
@@ -11,4 +11,18 @@ public class MidElem {
 //        System.out.println(slow.val);
 //        return slow;
 //    }
+
+
+    //delete mid node
+//    public static ListNode removeNthFromEndNAIVE(ListNode head) {
+//        if(head==null)return head;
+//        if(head.next==null)return null;
+//        ListNode slow=head, fast=head;
+//        while(fast.next.next!=null  && fast.next.next.next!=null){
+//            fast=fast.next.next;
+//            slow=slow.next;
+//        }
+//        slow.next=slow.next.next;
+//        return head;
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         int[] num2 = new int[]{-2, 3, 4, 6, -5};
         int[] num1 = new int[]{1, 3, -5};
         ListNode head = new ListNode(10);
-        head.next = new ListNode(20);
-        head.next.next = new ListNode(30);
-        head.next.next.next = new ListNode(40);
-        head.next.next.next.next = new ListNode(50);
-        ListNode teno = removeNthFromEndNAIVE(head, 5);
+//        head.next = new ListNode(20);
+//        head.next.next = new ListNode(30);
+//        head.next.next.next = new ListNode(40);
+//        head.next.next.next.next = new ListNode(50);
+        ListNode teno = removeNthFromEndNAIVE(head);
         while (teno != null) {
             System.out.println(teno.val);
             teno = teno.next;
