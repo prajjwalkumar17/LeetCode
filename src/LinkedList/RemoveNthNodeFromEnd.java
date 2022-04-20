d3b7e1979d4a7998652cea53ae13d55d1c6d0602
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-20 21:05:40 +0530

    Made the naive sol for deleting last k nodes from LL

---
+++ src/LinkedList/RemoveNthNodeFromEnd.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class RemoveNthNodeFromEnd {
+
+
+//    public static ListNode removeNthFromEndNAIVE(ListNode head, int n) {
+//        if(head==null)return head;
+//        int count=0;
+//        ListNode counter=head;
+//        while(counter!=null){
+//            count++;
+//            counter=counter.next;
+//        }
+//        int nodetodelete=count-n;
+//        ListNode temp = head;
+//        if(nodetodelete<count) {
+//            for (int i = 0; i < nodetodelete-1; i++) {
+//                temp = temp.next;
+//            }
+//            temp.next=temp.next.next;
+//        }
+//
+//        while(head!=null){
+//            System.out.println(head.val);
+//            head=head.next;
+//        }
+//
+//
+//        return head;
+//    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,41 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         int[] num2 = new int[]{-2, 3, 4, 6, -5};
         int[] num1 = new int[]{1, 3, -5};
         ListNode head = new ListNode(10);
-//        head.next=new ListNode(20);
-//        head.next.next=new ListNode(30);
-//        head.next.next.next=new ListNode(40);
-//        head.next.next.next.next=new ListNode(50);
-//        middleNode(head);
+        head.next = new ListNode(20);
+        head.next.next = new ListNode(30);
+        head.next.next.next = new ListNode(40);
+        head.next.next.next.next = new ListNode(50);
+        removeNthFromEndNAIVE(head, 1);
     }
 
+
+    public static ListNode removeNthFromEndNAIVE(ListNode head, int n) {
+        if (head == null) return head;
+        int count = 0;
+        ListNode counter = head;
+        while (counter != null) {
+            count++;
+            counter = counter.next;
+        }
+        int nodetodelete = count - n;
+        ListNode temp = head;
+        if (nodetodelete < count) {
+            for (int i = 0; i < nodetodelete - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+
+
+        return head;
+    }
 
 }
 
