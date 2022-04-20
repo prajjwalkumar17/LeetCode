6f89aaf44150b41b54e8370fa74c988e2ee1b6a0
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-20 20:25:58 +0530

    Made the mid elem of LinkedList

---
+++ src/LinkedList/MidElem.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class MidElem {
+//    public static ListNode middleNode(ListNode head) {
+//        if(head==null)return head;
+//        ListNode slow=head,fast=head;
+//        while(fast!=null && fast.next!=null){
+//            fast=fast.next.next;
+//            slow=slow.next;
+//        }
+//        System.out.println(slow.val);
+//        return slow;
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,23 @@ public class Main {
     public static void main(String[] args) {
         int[] num2 = new int[]{-2, 3, 4, 6, -5};
         int[] num1 = new int[]{1, 3, -5};
+        ListNode head = new ListNode(10);
+//        head.next=new ListNode(20);
+//        head.next.next=new ListNode(30);
+//        head.next.next.next=new ListNode(40);
+//        head.next.next.next.next=new ListNode(50);
+//        middleNode(head);
     }
 
 
 }
 
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
 
