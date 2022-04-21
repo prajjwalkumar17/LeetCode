1e7475e592e421a49273e6f04bcbf56cbcfdcc92
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-22 00:10:45 +0530

    Made the lumoto partition

--- src/Sorting/lumotoPartition.java
+++ src/Sorting/lumotoPartition.java
@@ -1,20 +1,6 @@
-import java.util.*;
-import java.util.List;
+package Sorting;
 
-public class Main {
-    public static void main(String[] args) {
-        int[] num = new int[]{10, 80, 30, 90, 40, 50, 70};
-//        int[] num1 = new int[]{1, 3, -5};
-//        ListNode head = new ListNode(10);
-//        head.next = new ListNode(20);
-//        head.next.next = new ListNode(30);
-//        head.next.next.next = new ListNode(40);
-//        head.next.next.next.next = new ListNode(50);
-
-        System.out.println((lumotoPartion(num, 0, num.length - 1)));
-
-    }
-
+public class lumotoPartition {
     private static int lumotoPartion(int[] arr, int low, int high) {
         int index = low - 1;
         //todo if a pivot is supplied in the params then swap high with pivot to make it at high
@@ -28,7 +14,7 @@ public class Main {
         //todo to swap the pivot to the specified i+1th pos as upto i there are less than eqaul to elems
         swap(arr, ++index, high);
         //todo return i+1
-        return index;
+        return index + 1;
     }
 
     private static void swap(int[] arr, int low, int high) {
@@ -36,16 +22,4 @@ public class Main {
         arr[low] = arr[high];
         arr[high] = temp;
     }
-
-
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    public ListNode(int val) {
-        this.val = val;
-    }
-}
-
