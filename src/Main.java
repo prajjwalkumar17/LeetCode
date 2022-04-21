51a323f59c2418129a893a28e13b059eeacc4b22
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-04-22 01:42:32 +0530

    Made the implementation of do while loop

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,11 @@
-import java.util.*;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        int[] num = new int[]{10, 80, 30, 90, 40, 50, 70};
-//        int[] num1 = new int[]{1, 3, -5};
-//        ListNode head = new ListNode(10);
-//        head.next = new ListNode(20);
-//        head.next.next = new ListNode(30);
-//        head.next.next.next = new ListNode(40);
-//        head.next.next.next.next = new ListNode(50);
+        int[] num = new int[]{5, 3, 8, 4, 2, 7, 1, 10};
 
-        System.out.println((lumotoPartion(num, 0, num.length - 1)));
+//        System.out.println((HoarePartion(num, 0, num.length - 1)));
 
     }
 
-    private static int lumotoPartion(int[] arr, int low, int high) {
-        int index = low - 1;
-        //todo if a pivot is supplied in the params then swap high with pivot to make it at high
-//        now continue the same array
-//        swap(arr[pivot],arr[high]);
-        int pivot = arr[high];
-        //todo swap all the elems that are low than pivot to inc the lessthan equalto windon
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j] < pivot) swap(arr, ++index, j);
-        }
-        //todo to swap the pivot to the specified i+1th pos as upto i there are less than eqaul to elems
-        swap(arr, ++index, high);
-        //todo return i+1
-        return index;
-    }
 
     private static void swap(int[] arr, int low, int high) {
         int temp = arr[low];
@@ -38,14 +14,5 @@ public class Main {
     }
 
 
-}
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    public ListNode(int val) {
-        this.val = val;
-    }
 }
 
---
+++ src/Sorting/HoarePartition.java
@@ -0,0 +1,25 @@
+package Sorting;
+
+public class HoarePartition {
+    static int HoarePartion(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+            do {
+                j--;
+            } while (arr[j] > pivot);
+            if (i >= j) return j;
+            swap(arr, i, j);
+        }
+    }
+
+    private static void swap(int[] arr, int low, int high) {
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+    }
+}
