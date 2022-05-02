7dabd41726068734f771c93a16d660fa86f8a1fb
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-02 12:51:47 +0530

    Backspace compare the string

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] num = new int[][]
-                {
-                        {1, 2, 3, 4},
-                        {5, 6, 7, 8},
-                        {9, 10, 11, 12},
-                        {13, 14, 15, 16}
-                };
+//        int[][] num = new int[][]
+//                {
+//                        {1, 2, 3, 4},
+//                        {5, 6, 7, 8},
+//                        {9, 10, 11, 12},
+//                        {13, 14, 15, 16}
+//                };
+        String s = "ab##", t = "c#d#";
+
+
     }
-
-
-//
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; j < arr[i].length; j++){
-//                System.out.print(arr[i][j] + " ");}
-//            System.out.println();
-//        }
-
-
 }
 
--- src/Test.java
+++ src/Test.java
@@ -4,9 +4,22 @@ import java.util.*;
 public class Test {
 
 
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int noofPeople = sc.nextInt();
+        int[] arr = new int[noofPeople];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(sc.next());
+            sum += arr[i];
+        }
+        int res = 0;
+        for (int i : arr) {
+            res += i * (sum - i);
+        }
 
-
+        System.out.println(res);
     }
 
 
