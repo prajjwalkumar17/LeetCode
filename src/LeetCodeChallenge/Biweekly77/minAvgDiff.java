2053dcceb670fa7b531ad9a89d10b46d0c41651a
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-03 13:02:36 +0530

    minavg diff problem of LCBW77

--- src/LeetCodeChallenge/Biweekly77/minAvgDiff.java
+++ src/LeetCodeChallenge/Biweekly77/minAvgDiff.java
@@ -40,6 +40,19 @@ public class minAvgDiff {
         }
         return resultIndex;
     }
-
+//    int len = nums.length, res = 0;
+//    long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
+//	for (int num : nums)
+//    sum += num;
+//	for (int i = 0; i < len; i++) {
+//        leftSum += nums[i];
+//        rightSum = sum - leftSum;
+//        long diff = Math.abs(leftSum / (i + 1) - (len - i == 1 ? 0 : rightSum / (len -i - 1)));
+//        if (diff < min) {
+//            min = diff;
+//            res = i;
+//        }
+//    }
+//	return res;
 
 }
