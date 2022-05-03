653b26d2182bd7874494fabede3ee039e1a53528
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-04 00:16:13 +0530

    Made the frequency of elements using hashmap

---
+++ src/Hashing/Frequency.java
@@ -0,0 +1,12 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class Frequency {
+    int[] nums = new int[]{2, 6, 2, 2, 3, 5, 1, 5};
+    HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int i:nums)
+//        map.put(i,map.getOrDefault(i,0)+1);
+
+//        System.out.println(map);
+}
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Integer[] nums = new Integer[]{2, 6, 2, 2, 3, 5, 1, 5};
+        int[] nums = new int[]{2, 6, 2, 2, 3, 5, 1, 5};
 
 
     }
