93f0b37e0c657ee48fcb457b820f409c93e93cbf
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-05 22:51:36 +0530

    Made the eff solution for Longest subarray with given sum the points to remember is 1. we don't need to overwrite elements and add element only when there is no prev elem with curr sum is present 2. in case of accc == sum we need to just update the res as at that time it will be the longest

--- src/Hashing/LongestSubArrayWithGivenSum.java
+++ src/Hashing/LongestSubArrayWithGivenSum.java
@@ -1,6 +1,28 @@
 package Hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithGivenSum {
+
+    private static int longestSubarrayWithGivenSumEFF(int[] arr, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int accumulator = 0, maxLength = 0;
+        for (int i = 0; i < arr.length; i++) {
+            accumulator += arr[i];
+            //Check if accum is sum then no comp needed it will be biggest in present
+            if (accumulator == sum) maxLength = i + 1;
+            if (map.containsKey(accumulator - sum))
+                maxLength = Math.max(maxLength, i - map.get(accumulator - sum));
+            //Don't replace the old key value add only if previously it isn't present
+            if (!map.containsKey(accumulator))
+                map.put(accumulator, i);
+        }
+        System.out.println(maxLength);
+        return maxLength;
+    }
+
+
     private static int longestSubarrayWithGivenSumNAIVE(int[] arr, int sum) {
         int length = 0;
         for (int i = 0; i < arr.length; i++) {
--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int[] nums1 = new int[]{5, 8, -4, -4, 9, -2, -2};
-        int sum = 0;
+        int[] nums1 = new int[]{8, 3, 1, 5, -6, 6, 2, 2};
+        int sum = 4;
         longestSubarrayWithGivenSum(nums1, sum);
 
     }
 
-    private static int longestSubarrayWithGivenSum(int[] arr, int sum) {
-        return 0;
-    }
-
 
 }
+
+
+
 
