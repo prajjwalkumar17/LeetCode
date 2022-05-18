e5d920cfa86c91288ea0af7b43e674017510e3d3
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-05-19 01:29:58 +0530

    again starting ds after 11 days due to internals VALORANT and my birthday it was really sick to stay for so long but at last I am here

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
         int[] nums1 = new int[]{0, 1, 0, 0, 0, 0};
         int[] nums2 = new int[]{1, 0, 1, 0, 0, 1};
-        subArraySameIndexedSum(nums1, nums2);
 
     }
 
