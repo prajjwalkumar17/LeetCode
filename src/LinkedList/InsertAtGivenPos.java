2b2ebfaf1d52aeca77833248650c4adc3200bcb3
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-08 02:12:48 +0530

    made the insert at pos of linkedlist

---
+++ src/LinkedList/InsertAtGivenPos.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+public class InsertAtGivenPos {
+//    private static Main.Node insertAtPos(Main.Node head, int data, int pos) {
+//        Main.Node temp=head;
+//        int i=0;
+//        Main.Node res=new Main.Node(data);
+//        while(temp!=null){
+//            temp=temp.next;
+//            i++;
+//        }
+//        if(pos==1){
+//            //if the pos is head
+//            res.next=head;
+//            return res;
+//        }
+//        //legal pos
+//        //if the pos is something diff than head\
+//        Main.Node tem = head;
+//        for (int j = 1; j<= pos-2 && tem!=null; j++)
+//            tem = tem.next;
+//
+//        if(tem==null)return head;
+//        res.next = tem.next;
+//        tem.next = res;
+//        return head;
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,53 @@
 import java.util.Arrays;
 
 public class Main {
-    static final int CHAR = 256;
-
     public static void main(String[] args) {
-//        Node head = new Node(10);
-//        head.next = new Node(20);
-//        head.next.next = new Node(30);
-//        printlist(deleteAtEnd(head));
-
-
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        printlist(insertAtPos(head, 2, 4));
     }
 
+    private static Node insertAtPos(Node head, int data, int pos) {
+        Node temp = head;
+        int i = 0;
+        Node res = new Node(data);
+        while (temp != null) {
+            temp = temp.next;
+            i++;
+        }
+        if (pos == 1) {
+            //if the pos is head
+            res.next = head;
+            return res;
+        }
+        //legal pos
+        //if the pos is something diff than head\
+        Node tem = head;
+        for (int j = 1; j <= pos - 2 && tem != null; j++)
+            tem = tem.next;
 
-//    private static Node deleteAtEnd(Node head) {
-//        if (head == null || head.next == null) return null;
-//        Node temp = head;
-//        while (temp.next.next != null) {
-//            temp = temp.next;
-//        }
-//        temp.next = null;
-//        return head;
-//    }
-//
-//    static void printlist(Node list) {
-//        if (list == null) return;
-//        System.out.println(list.data);
-//        printlist(list.next);
-//    }
-//
-//    public static class Node {
-//        int data;
-//        Node next;
-//
-//        public Node(int data) {
-//            this.data = data;
-//            this.next = null;
-//        }
-//    }
+        if (tem == null) return head;
+        res.next = tem.next;
+        tem.next = res;
+        return head;
+    }
+
+    static void printlist(Node list) {
+        if (list == null) return;
+        System.out.println(list.data);
+        printlist(list.next);
+    }
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
 }
 
