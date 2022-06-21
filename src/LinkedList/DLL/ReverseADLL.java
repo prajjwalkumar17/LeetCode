04c655e5c8aab289e7944e17594aa3ae725213ad
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-21 20:20:31 +0530

    Made the reversing of DLL

---
+++ src/LinkedList/DLL/ReverseADLL.java
@@ -0,0 +1,16 @@
+package LinkedList.DLL;
+
+public class ReverseADLL {
+//    private static Node reverseDLL(Node head){
+//        if(head==null||head.next==null)return head;
+//        Node prev=null;
+//        while(head!=null){
+//            prev=head.prev;
+//            head.prev=head.next;
+//            head.next=prev;
+//            head=head.prev;
+//        }
+//        return prev.prev;
+//    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         Node head = new Node(10);
-        head.next = new Node(20);
-        head.next.next = new Node(30);
-        head.next.next.next = new Node(40);
-        head.next.next.next.next = new Node(50);
-        printlist(deleteEndNode(head));
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+        Node temp3 = new Node(40);
+        head.next = temp1;
+        temp1.prev = head;
+        temp1.next = temp2;
+        temp2.prev = temp1;
+        temp2.next = temp3;
+        temp3.prev = temp2;
+//        printlist(reverseDLL(head));
     }
 
-    public static Node deleteEndNode(Node head) {
-        if (head == null) return null;
-        if (head.next == null) return null;
-        Node trav = head;
-        while (trav.next != null)
-            trav = trav.next;
-        trav.prev.next = null;
-        return head;
-    }
 
     static void printlist(Node list) {
         if (list == null) return;
