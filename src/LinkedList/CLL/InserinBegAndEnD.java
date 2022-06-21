31dcf1c8d789dc4dabb47f48203227e36f220f04
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-21 21:46:02 +0530

    Made the insert at beg and insert at end of CLL

---
+++ src/LinkedList/CLL/InserinBegAndEnD.java
@@ -0,0 +1,56 @@
+package LinkedList.CLL;
+
+public class InserinBegAndEnD {
+
+    //SWAP the nodes values after inserting the node after head
+//    private static Main.Node insertAtBEGEFF(Main.Node head, int data) {
+//        Main.Node temp=new Main.Node(data);
+//        if(head==null){
+//            temp.next=temp;
+//            return temp;
+//        }
+//        else {
+//            int headData=head.data;
+//            temp.next=head.next;
+//            head.next=temp;
+//            head.data=temp.data;
+//            temp.data=headData;
+//            return head;
+//        }
+//    }
+//    private static Node insertAtBEG(Node head, int data) {
+//        Node temp=new Node(data);
+//        if(head==null){
+//            temp.next=temp;
+//            return temp;
+//        }
+//        else {
+//            Node tail = head;
+//            while (tail.next != head)
+//                tail = tail.next;
+//            tail.next = temp;
+//            tail.next.next = head;
+//            return tail.next;
+//        }
+//    }
+
+    //same as insert at beg we are changing head
+
+//    private static Main.Node insertAtBEGEFF(Main.Node head, int data) {
+//        Main.Node temp=new Main.Node(data);
+//        if(head==null){
+//            temp.next=temp;
+//            return temp;
+//        }
+//        else {
+//            int headData=head.data;
+//            temp.next=head.next;
+//            head.next=temp;
+//            head.data=temp.data;
+//            temp.data=headData;
+//            return head.next;
+//        }
+//    }
+
+
+}
