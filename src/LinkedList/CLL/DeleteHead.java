3a3a03a5a22e65156d7f24424d083e0d8d2e826d
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-22 22:59:27 +0530

    Made the deletion of head of CLL

---
+++ src/LinkedList/CLL/DeleteHead.java
@@ -0,0 +1,22 @@
+package LinkedList.CLL;
+
+public class DeleteHead {
+
+//    private static Main.Node deleteHeadEFF(Main.Node head) {
+//        if(head==null||head.next==head)return null;
+//        head.data=head.next.data;
+//        head.next=head.next.next;
+//        return head;
+//    }
+//
+
+//    private static Main.Node deleteHeadNAIVE(Main.Node head) {
+//        if(head==null||head.next==head)return null;
+//        Main.Node temp=head;
+//        while(temp.next!=head)
+//            temp=temp.next;
+//        temp.next=head.next;
+//        return head.next;
+//    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,14 @@ public class Main {
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
         head.next.next.next.next = head;
-        printCLL(insertAtBEGEFF(head, 0));
+        printCLL(deleteHead(head));
     }
 
-    private static Node insertAtBEGEFF(Node head, int data) {
-        Node temp = new Node(data);
-        if (head == null) {
-            temp.next = temp;
-            return temp;
-        } else {
-            int headData = head.data;
-            temp.next = head.next;
-            head.next = temp;
-            head.data = temp.data;
-            temp.data = headData;
-            return head;
-        }
+    private static Node deleteHeadEFF(Node head) {
+        if (head == null || head.next == head) return null;
+        head.data = head.next.data;
+        head.next = head.next.next;
+        return head;
     }
 
     private static void printCLL(Node head) {
