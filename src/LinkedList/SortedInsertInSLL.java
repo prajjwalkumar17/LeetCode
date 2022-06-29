2cdf068328fb4b0aa334fbf48a27047d16f516d5
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-29 22:49:49 +0530

    made the sorted insert problem in linked list

---
+++ src/LinkedList/SortedInsertInSLL.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class SortedInsertInSLL {
+
+//    private static Node SortedInsert(Node head, int k) {
+//        Node toAdd = new Node(k);
+//        if (head == null) return toAdd;
+//        if (k < head.data) {
+//            toAdd.next = head;
+//            return toAdd;
+//
+//        }
+//        Node temp = head;
+//        while (temp.next != null && temp.next.data < k)
+//            temp = temp.next;
+//        toAdd.next = temp.next;
+//        temp.next = toAdd;
+//        return head;
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,36 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Node head = new Node(10);
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
-        head.next.next.next.next = head;
-        printCLL(deletekthNode(head, 2));
+        head.next.next.next.next = new Node(50);
+        printSLL(SortedInsert(head, 25));
     }
 
-    private static Node deletekthNode(Node head, int k) {
-        if (head == null || head.next == head) return null;
-        if (k == 1) {
-            head.data = head.next.data;
-            head.next = head.next.next;
-            return head;
-        } else {
-            Node temp = head;
-            for (int i = 0; i < k - 2; i++)
-                temp = temp.next;
-            temp.next = temp.next.next;
-            return head;
+    private static Node SortedInsert(Node head, int k) {
+        Node toAdd = new Node(k);
+        if (head == null) return toAdd;
+        if (k < head.data) {
+            toAdd.next = head;
+            return toAdd;
+
         }
+        Node temp = head;
+        while (temp.next != null && temp.next.data < k)
+            temp = temp.next;
+        toAdd.next = temp.next;
+        temp.next = toAdd;
+        return head;
     }
 
-    private static void printCLL(Node head) {
+    private static void printSLL(Node head) {
         if (head == null) return;
         Node curr = head;
-        do {
+        while (curr != null) {
             System.out.println(curr.data);
             curr = curr.next;
-        } while (curr != head);
+        }
     }
 
 
