88cd84daac99f9f3781bbf04e3d44315398f7e51
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-09-11 02:13:30 +0530

    removed duplicates

---
+++ src/LinkedList/RemoveDuplicates.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class RemoveDuplicates {
+
+//    public static ListNode rremoveDuplicate(ListNode node){
+//        ListNode res=node;
+//        while(node.next!=null){
+//            if(node.val==node.next.val) node.next=node.next.next;
+//            else
+//                node=node.next;
+//        }
+//        return res;
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         ListNode node = new ListNode(7);
         node.next = new ListNode(9);
-        node.next.next = new ListNode(2);
-        node.next.next.next = new ListNode(10);
+        node.next.next = new ListNode(9);
+        node.next.next.next = new ListNode(9);
         node.next.next.next.next = new ListNode(1);
-        node.next.next.next.next.next = new ListNode(8);
+        node.next.next.next.next.next = new ListNode(1);
         node.next.next.next.next.next.next = new ListNode(6);
 
-//        ListNode res = reverseSpecified(node, 1,7);
-//        while (res != null) {
-//            System.out.println(res.val);
-//            res = res.next;
-//        }
+        ListNode res = rremoveDuplicate(node);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
     }
 
+    public static ListNode rremoveDuplicate(ListNode node) {
+        ListNode res = node;
+        while (node.next != null) {
+            if (node.val == node.next.val) node.next = node.next.next;
+            else
+                node = node.next;
+        }
+        return res;
+    }
 
     public static class ListNode {
         int val;
