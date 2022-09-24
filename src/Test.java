46ebccb89e3ba86bf51b8501453280224dec1709
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-09-24 06:58:01 +0530

    Started graph

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,28 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Test {
+    //constants
+//     static int a=2;
+
+    int c = 10;
+
     public static void main(String[] args) throws IOException {
+        int p = 5;
+        int y = 10;
+        int z = 15;
+        int x = 20;
+        int e = res(p, y, z, x);
+        System.out.println(e);
+
+
     }
 
-
+    public static int res(int a, int b, int c, int d) {
+        int e = (a + b) / (c * d);
+        return e;
+    }
 }
+
+
 
 
