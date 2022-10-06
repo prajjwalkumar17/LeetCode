c818ba0470f8f8ac7773021681d6b2b6797ffcbb
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-10-06 20:42:02 +0530

    made the cycle detection in directed graph using the khan's algorithm that uses a bfs traversal

---
+++ src/Graph/CycleDirected2.java
@@ -0,0 +1,34 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CycleDirected2 {
+    private static boolean cycleDetection2(ArrayList<ArrayList<Integer>> list, int nofV) {
+        int inDegree[] = new int[nofV];
+        int count = 0;
+
+        //TODO constructing the indegree array
+        for (int i = 0; i < nofV; i++)
+            for (int u : list.get(i))
+                inDegree[u]++;
+
+        //Initializing queue and adding all indegree 0 in it
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < nofV; i++)
+            if (inDegree[i] == 0)
+                q.add(i);
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            count++;
+            for (int neighbour : list.get(curr))
+                if (--inDegree[neighbour] == 0)
+                    q.add(neighbour);
+
+
+        }
+        return count != nofV;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        int nofV = 5;
+        int nofV = 4;
         for (int i = 0; i < nofV; i++)
             list.add(new ArrayList<Integer>());
-        addEdge(list, 0, 2);
-        addEdge(list, 0, 3);
-        addEdge(list, 1, 3);
-        addEdge(list, 1, 4);
+        addEdge(list, 0, 1);
+        addEdge(list, 1, 2);
         addEdge(list, 2, 3);
-        topologicalSort(list, nofV);
+        System.out.println(cycleDetection2(list, nofV));
     }
 
-    private static void topologicalSort(ArrayList<ArrayList<Integer>> list, int nofV) {
-        int inDegree[] = new int[nofV];
-        for (int u = 0; u < nofV; u++)
-            for (int node : list.get(u))
-                inDegree[node]++;
 
-        Queue<Integer> q = new LinkedList<>();
-        //Adding all the 0 indegree nodes to q at beginning
-        for (int i = 0; i < nofV; i++)
-            if (inDegree[i] == 0)
-                q.add(i);
-
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            System.out.print(curr + " ");
-            for (int i : list.get(curr))
-                if (--inDegree[i] == 0)
-                    q.add(i);
-        }
-
-    }
 
 
     private static void printGraph(ArrayList<ArrayList<Integer>> list, int v) {
