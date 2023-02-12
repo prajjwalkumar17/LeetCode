dcb0ffdd5907710fa773ddf6452ae20ca934f393
prajjwalkumar17 <talk2prajjwal@gmail.com>
2023-02-12 23:38:39 +0530

    Now till start again Dt-: 12.02.2023

---
+++ src/DynamicProgramming/knapsack01.java
@@ -0,0 +1,26 @@
+package DynamicProgramming;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class knapsack01 {
+    static int[][] dp = new int[102][102];
+/*    public static void main(String[] args) {
+        for(int row[]:dp)
+            Arrays.fill(row,-1);
+        int[] wt=new int[]{1,2,4,5};
+        int[] val=new int[]{3,2,1,6};
+        int W=7;
+        int n=wt.length;
+        System.out.println(knapsack(wt,val,W,n));
+    }*/
+/*
+    private static int knapsack(int[] wt, int[] val, int W, int n) {
+        if(n<0||W<0)return 0;
+        if(dp[n][W]!=-1)return dp[n][W];
+
+    }
+*/
+
+
+}
